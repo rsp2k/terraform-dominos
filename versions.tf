903090930b7f61792c9a9ceec4a4317a2feec77b
Ryan Malloy <rpm@malloys.us>
2025-04-27 16:20:50 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,7 @@
 terraform {
-  required_version = ">= 1.0"
-
   required_providers {
     dominos = {
-      source  = "ncr-devops-platform/dominos"
-      version = "~> 0.3" # Use the appropriate version
+      source = "MNThomson/dominos"
     }
   }
 }
